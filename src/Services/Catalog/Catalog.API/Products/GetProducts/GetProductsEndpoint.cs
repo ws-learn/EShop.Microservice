b277cf9dbf34439c2df0e5fd1cf6702ab89ecc51
diduboyz <wlsn_ting520@hotmail.com>
2025-09-14 23:04:59 +0800

    add GetProduct handler and query

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+
+namespace Catalog.API.Products.GetProduct;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
