ee2590f375d3b2b09d404ba306aa7a930a86d7b0
diduboyz <wlsn_ting520@hotmail.com>
2025-09-26 08:16:49 +0800

    add EF Migration Script

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+using Discount.Grpc.Proto;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : Proto.DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
+
