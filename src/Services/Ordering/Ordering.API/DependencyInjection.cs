aa46fbf707835a664f8643c32503c22f8449f75e
diduboyz <wlsn_ting520@hotmail.com>
2025-10-11 16:08:04 +0800

    added DependencyInjection.cs file to all Projects

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddCarter();
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.AddCarter();
+        return app;
+    }
+}
