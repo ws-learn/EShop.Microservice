1e7db42a8739258b2323973f1188911c19778eb2
diduboyz <wlsn_ting520@hotmail.com>
2025-10-11 16:10:01 +0800

    add

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,18 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services
+     .AddApplicationServices(builder.Configuration)
+     .AddInfrastructureServices(builder.Configuration)
+     .AddApiServices(builder.Configuration);
+
+var app = builder.Build();
+
+//Configure the HTTP Request pipeline
+
+app.Run();
+
