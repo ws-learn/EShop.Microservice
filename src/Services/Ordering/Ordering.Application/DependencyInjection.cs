56ce3d830d14c64187c7556208049996717594fd
diduboyz <wlsn_ting520@hotmail.com>
2025-10-18 15:03:50 +0800

    Add SaveChangesInterceptor

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +9,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices
         (this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddMediatR(r =>
+        {   
+            r.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
         return services;
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure;
 
@@ -13,11 +15,18 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Database");
         
         // Add Services to the container
-        services.AddDbContext<ApplicationDbContext>(options => 
-             options.UseSqlServer(connectionString));
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+            
         
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
-        
+            
         return services;
     }
 }
