88acb35ddeb356593726379e5a041fac639622a7
diduboyz <wlsn_ting520@hotmail.com>
2025-11-16 15:20:45 +0800

    Containerization of Ordering.MS and RabbitMQ

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -12,7 +12,7 @@ public record OrderName
     public static OrderName Of(string value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(value);
-        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length,DefaultLength);
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length,DefaultLength);
         return new OrderName(value);
     }
 }
