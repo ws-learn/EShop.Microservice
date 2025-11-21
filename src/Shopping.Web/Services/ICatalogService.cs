c685f33ca2e88c317a25a2a007c5ed9a8f1e2041
diduboyz <wlsn_ting520@hotmail.com>
2025-11-21 22:55:17 +0800

    Add business layer for shopping.application

---
+++ src/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,14 @@
+using Refit;
+using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+    Task<GetProductsResponse> GetProducts(int? PageNumber = 1, int? PageSize = 10);
+    [Get("/catalog-service/products/{id}")]
+    Task<GetProductByIdResponse> GetProduct(Guid id);
+    [Get("/catalog-service/products/category/{category}")]
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+}
